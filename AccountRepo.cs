830fac3c1dd4292fbe0a2d207069d3cd45b74020
Benjamin Fischer <benjamin.fischer.unity@gmail.com>
2021-07-28 12:22:35 +0200

    added to git

---
+++ AccountRepo.cs
@@ -0,0 +1,25 @@
+namespace AuthServer
+{
+    static class AccountRepo
+    {
+
+        private static string Hash(string value)
+        {
+            return "";
+        }
+
+
+        public static string Login(string email, string password)
+        {
+
+
+            return "";
+        }
+
+        public static string Create(string email, string password)
+        {
+
+            return "";
+        }
+    }
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using AuthServer.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            try
+            {
+                CreateAccountDTO dto = payload.ToObject<CreateAccountDTO>();           
+            } catch (JsonSerializationException e)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
