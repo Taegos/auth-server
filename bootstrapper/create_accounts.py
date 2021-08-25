2e94d12b65b1aad9b675cec466d2031b3365ba4b
Benjamin Fischer <benjamin-fischer@outlook.com>
2021-08-25 17:53:11 +0200

    Don't crash on duplicates

--- bootstrapper/create_accounts.py
+++ bootstrapper/create_accounts.py
@@ -2,13 +2,16 @@ from models.account import Account
 import bcrypt
 
 def _create_account(display_name: str):
-    Account.create(
-        email=f'{display_name.lower()}@example.com',
-        display_name=display_name,
-        password_hash=bcrypt.hashpw("123".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), 
-        email_confirmed=True,
-        is_admin=True
-    )
+    try:
+        Account.create(
+            email=f'{display_name.lower()}@example.com',
+            display_name=display_name,
+            password_hash=bcrypt.hashpw("123".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), 
+            email_confirmed=True,
+            is_admin=True
+        )
+    except Exception:
+        pass
 
 def create_accounts(*display_names):
     for display_name in display_names:
