19c911aefaac3d06950da528efc1c92319a159a6
Benjamin Fischer <benjamin-fischer@outlook.com>
2021-08-25 18:37:44 +0200

    Setup

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ from flask import Flask
 from models.database import get_database, init_database
 from config import LocalConfig, HerokuConfig
 
-def _create_tables(): 
+def _setup_db(): 
     from models.account import Account
     from peewee import IntegrityError
+    from bootstrapper.create_accounts import create_example_accounts
+
     try:
       #  if not is_deployed_to_heroku():
         get_database().drop_tables([Account])
         get_database().create_tables([Account])
+        create_example_accounts()
     except Exception as e:
         print("EXCEPTION " + str(e))
 
 def create_app(config: object):
     init_database(config) # Needs to come before anything else
 
-    _create_tables()
-    #from bootstrapper.create_accounts import create_example_accounts
-    #create_example_accounts()
+    _setup_db()
 
     from blueprints.account import account
     from blueprints.auth_token import auth_token
